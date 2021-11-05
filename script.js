4f5147d1c4f0b4f9c9132ab658a663bdbf04b243
Kenyon Huppe <kenyonhuppe@gmail.com>
2021-11-04 22:30:06 -0700

    Progress On Edit Functionality

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const LOCAL_STORAGE_LIST_KEY = 'task.list'
 const LOCAL_STORAGE_ARRAY_KEY = 'task.array'
 
 let dataList = document.getElementById('data-list');
+let editButton = document.getElementById('edit-btn');
 
 // ! may not need array?
 let list = new Array();
@@ -44,7 +45,7 @@ function addEntry(e) {
     list.push(entryValue);
     console.log(list);
 
-    dataList.innerHTML += `<li id="${list.length}"><span class="bubble">&#9711</span>${entryValue}</li>`;
+    dataList.innerHTML += `<li id="${list.length}"><span class="bubble">&#9711</span>${list[(list.length - 1)]}<button class="edit-btn" id="-${list.length}">Edit</button></li>`;
 
     let output = dataList.innerHTML;
     // saves output at end of each added entry
@@ -56,13 +57,83 @@ function addEntry(e) {
 }
 
 // todo 
-function editEntry() {
+dataList.addEventListener('click', editEntry);
+
+function editEntry(e) {
+    // disallows multiple entries from being selected for edit
+    console.log('this');
+    list = loadLocalStorage(localStorage.getItem(LOCAL_STORAGE_ARRAY_KEY));
+    // grabs given element id
+    let selectedEntryId = '';
+    // makes sure it's correct tag
+    if (e.target.tagName.toLowerCase() === 'button') {
+        selectedButtonId = e.target.getAttribute('id');
+        console.log(selectedButtonId);
+
+        // allows user to click in dataList area without interuption of event listener
+        dataList.removeEventListener('click', editEntry);
+
+        // the button value is negative version while entry value is positive version. To get entry, must make id positive version of itself
+        let selectedEntryId = (selectedButtonId * -1);
+
+        let editValue = document.getElementById(selectedEntryId);
+
+        console.log(list[(selectedEntryId - 1)]);
+        // changes to textbox where user can enter new edit
+        editValue.innerHTML = `<form action="" class="form" id="entry-form">
+        <button class="btn" id="entry-btn" type="submit"><i
+                class="fas fa-solid fa-plus"></i></button>
+        <input type="text" id="entry-value" placeholder="Enter To-Do Item">`
+
+        // grabs new edit
+        document.getElementById(selectedEntryId).addEventListener('submit', () => {
+            e.preventDefault();
+            // grabs entered value
+            let entryValue = document.getElementById('entry-value').value;
+            // if enmpy value is submitted
+            if (entryValue == null || entryValue === '') {
+                // turns back on event listener
+                dataList.addEventListener('click', editEntry);
+                // reloads entry item
+                editValue.innerHTML = `<li id="${selectedEntryId}"><span class="bubble">&#9711</span>${list[(selectedEntryId - 1)]}<button class="edit-btn" id="-${list.length}">Edit</button></li>`;
+                return;
+            }
+
+            // todo automatically have cursor in textbox
+
+            // todo look into setting html to a variable
+
+            list[(selectedEntryId - 1)] = entryValue;
+            console.log('select' + selectedEntryId);
+            let output = '';
+
+            for (let i in list) {
+                output += `<li id="${list[i]}"><span class="bubble">&#9711</span>${list[i]}<button class="edit-btn" id="-${list[i]}">Edit</button></li>`;
+                // if(i === (selectedEntryId - 1){}
+                console.log(list[i]);
+            }
+
+            // dataList.innerHTML = output;
+            save(output, list);
+
+            //// returns to list entry
+            // editValue.innerHTML = `<li id="${selectedEntryId}"><span class="bubble">&#9711</span>${entryValue}<button class="edit-btn" id="-${list.length}">Edit</button></li>`;
+
+            // re-activates event listener
+            dataList.addEventListener('click', editEntry);
+        })
+    } else {
+        console.log('it is because of that');
+    }
+
+
+
     /*
         todo if user clicks on entry (all have same class name with even listener), then go into editEntry funciton
-
+ 
         todo *** make the edit button a type submit, then grab id ***
-
-
+ 
+ 
         todo run through array that goes through all id's 
     */
 
@@ -76,16 +147,21 @@ function deleteEntry() {
 
 // clears all to-do items
 function clearAll(output, list) {
+
     dataList.innerHTML = '';
     output = '';
     localStorage.clear();
+    // allows to use edit feature without issues
+    window.location.reload(true);
 }
 
 // saves to local storage
 function save(output, list) {
     // saves dataList HTML info
+    // !
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, output);
     // saves list
+    console.log(output);
 
     localStorage.setItem(LOCAL_STORAGE_ARRAY_KEY, JSON.stringify(list));
 }
@@ -99,7 +175,8 @@ function loadLocalStorage() {
     for (let i in tempVar) {
         newList.push(tempVar[i]);
     }
-
+    console.log('output' + localStorage.getItem(LOCAL_STORAGE_ARRAY_KEY));
+    console.log('array' + localStorage.getItem(LOCAL_STORAGE_ARRAY_KEY));
     return newList;
 }
 /*
